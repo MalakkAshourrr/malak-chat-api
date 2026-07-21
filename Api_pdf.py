952c5d90ba132039fd87277ca1baa125c05f58e6
MalakkAshourrr <168218440+MalakkAshourrr@users.noreply.github.com>
2026-07-21 16:37:34 +0300

    Update Api_pdf.py

--- Api_pdf.py
+++ Api_pdf.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # AI Client
 # -----------------------
 client = OpenAI(
-    api_key="api_gAAAAABqV53NXXTPzmZ-4KS2gqrPaRUvT_lgMHDmePjtNPk2JXQBW53cPtkhwBtKyt0a42O2DVQqLKnbp2zy2sIa0qphBhXiK4mEa2tOGmOG_J2Gb3quyU4R6cKrYlrRFuOS0hBVohnE",
+    api_key="api_J2Gb3quyU4R6cKrYlrRFuOS0hBVohnE",
     base_url="https://api-pilot-sandbox.aurai.solutions/v1"
 )
 
