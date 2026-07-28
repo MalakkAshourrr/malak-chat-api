d2dc3e4c66db2e3c0b140338d1a0d910bf7d253f
MalakkAshourrr <168218440+MalakkAshourrr@users.noreply.github.com>
2026-07-28 14:56:28 +0300

    Update basee64.py

--- basee64.py
+++ basee64.py
@@ -6,7 +6,7 @@ from openai import OpenAI
 app = FastAPI()
 
 client = OpenAI(
-    api_key="api_gAAAAABqV53NXXTPzmZ-4KS2gqrPaRUvT_lgMHDmePjtNPk2JXQBW53cPtkhwBtKyt0a42O2DVQqLKnbp2zy2sIa0qphBhXiK4mEa2tOGmOG_J2Gb3quyU4R6cKrYlrRFuOS0hBVohnE",
+    api_key="api_g",
     base_url="https://api-pilot-sandbox.aurai.solutions/v1"
 )
 
