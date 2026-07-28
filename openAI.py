ffd10923e6fae841a75fba990107c8e8d8aca24a
MalakkAshourrr <168218440+MalakkAshourrr@users.noreply.github.com>
2026-07-28 14:56:46 +0300

    Update openAI.py

--- openAI.py
+++ openAI.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 client = OpenAI(
-    api_key="api_gAAAAABqV53NXXTPzmZ-4KS2gqrPaRUvT_lgMHDmePjtNPk2JXQBW53cPtkhwBtKyt0a42O2DVQqLKnbp2zy2sIa0qphBhXiK4mEa2tOGmOG_J2Gb3quyU4R6cKrYlrRFuOS0hBVohnE",
+    api_key="api_gAAAAABqV53NXXTPzmZ-4KS",
     base_url="https://api-pilot-sandbox.aurai.solutions/v1"
 )
 
